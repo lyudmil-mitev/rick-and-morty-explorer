fcdb113e3937aa5eb174b03dbb9435a3ca07d624
lyudmil-mitev <lyudmil@mitev.me>
2026-06-18 01:11:51 +0200

    Render episode detail data from loader

--- src/routes/EpisodeDetails.tsx
+++ src/routes/EpisodeDetails.tsx
@@ -1,10 +1,7 @@
-import { Character, Episode } from "rickmortyapi";
 import DetailsLayout, { DetailFacts } from "../components/DetailsLayout";
 import { Link, useLoaderData } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { parseAPIId } from "../loaders";
-import { fetchApiResources } from "../api";
 import CharacterCard from "../components/CharacterCard";
+import { EpisodeDetailsLoaderData } from "../loaders";
 
 import Season1 from "/seasons/s01.jpg";
 import Season2 from "/seasons/s02.jpg";
@@ -12,14 +9,12 @@ import Season3 from "/seasons/s03.jpg";
 import Season4 from "/seasons/s04.jpg";
 import Season5 from "/seasons/s05.jpg";
 import Season6 from "/seasons/s06.jpg";
-import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function EpisodeDetails() {
-    const episode = useLoaderData() as Episode;
+    const { episode, characters } = useLoaderData() as EpisodeDetailsLoaderData;
     const season = parseInt(episode.episode.slice(2, 3), 10);
     const images = [Season1, Season2, Season3, Season4, Season5, Season6];
     const image = season <= 6 ? images[season - 1] : images[0];
-    const [characters, setCharacters] = useState<Character[]>([]);
 
     const DetailFacts: DetailFacts[] = [
         { type: "Episode", value: episode.episode },
@@ -27,18 +22,13 @@ export default function EpisodeDetails() {
         { type: "Characters", value: `${episode.characters.length} characters` },
     ]
 
-    useEffect(() => {
-        const characterList = episode.characters.map((character) => parseAPIId(character));
-        fetchApiResources<Character>('character', characterList).then(setCharacters)
-    }, [episode.characters])
-
     return (
         <DetailsLayout title={episode.name} image={image} facts={DetailFacts} childrenTitle={"Characters"}>
-            {characters.length > 0 ? characters.map((character, index) => (
-                <Link to={`/characters/${character.id}`} key={index}>
+            {characters.map((character) => (
+                <Link to={`/characters/${character.id}`} key={character.id}>
                     <CharacterCard character={character} />
                 </Link>
-            )) : <><LoadingSpinner/> Loading...</>}
+            ))}
         </DetailsLayout>
     )
 }
