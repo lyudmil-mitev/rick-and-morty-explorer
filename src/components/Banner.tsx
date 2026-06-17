d75e6815a183e5f4e38069078d9823c71692935f
lyudmil-mitev <lyudmil@mitev.me>
2026-06-18 00:13:26 +0200

    Restore banner alignment and sizing

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,23 @@
-
 import mortyLogo from '/android-chrome-512x512.png'
 import starrySpace from '/StarrySpace.svg'
 
-const Banner = () => <section className="flex justify-center items-center bg-cover bg-center py-20" style={{ backgroundImage: `url(${starrySpace})` }}>
-    <a title="Powered by Rick and Morty API!" href="https://rickandmortyapi.com/" target="_blank">
-        <img src={mortyLogo} className="logo" alt="Morty Logo" />
+const Banner = () => (
+  <section
+    className="flex flex-col items-center justify-center bg-cover bg-center px-4 py-10 sm:flex-row sm:py-14"
+    style={{ backgroundImage: `url(${starrySpace})` }}
+  >
+    <a
+      title="Powered by Rick and Morty API!"
+      href="https://rickandmortyapi.com/"
+      target="_blank"
+    >
+      <img src={mortyLogo} className="logo" alt="Morty Logo" />
     </a>
-    <h1 className="schwifty text-6xl sm:text-10xl">Rick and Morty Explorer</h1>
-</section>
 
-export default Banner;
+    <h1 className="schwifty text-center text-5xl sm:text-7xl lg:text-8xl">
+      Rick and Morty Explorer
+    </h1>
+  </section>
+)
+
+export default Banner
