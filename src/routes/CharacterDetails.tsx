3598089405b71f48834fd80423ffeedebe68be6b
lyudmil-mitev <lyudmil@mitev.me>
2026-06-18 01:11:37 +0200

    Render character detail data from loader

--- src/routes/CharacterDetails.tsx
+++ src/routes/CharacterDetails.tsx
@@ -1,38 +1,29 @@
-import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
-import { Character, Episode } from "rickmortyapi";
-import { parseAPIId } from "../loaders";
-import { fetchApiResources } from "../api";
 import EpisodeCard from "../components/EpisodeCard";
 import DetailsLayout, { DetailFacts } from "../components/DetailsLayout";
-import LoadingSpinner from "../components/LoadingSpinner";
+import { CharacterDetailsLoaderData } from "../loaders";
+import { parseApiUrlId } from "../api";
 
 export default function CharacterDetails() {
-    const char = useLoaderData() as Character;
-    const [episodes, setEpisodes] = React.useState<Episode[]>([]);
-    const originId = parseAPIId(char.origin.url);
-    const locationId = parseAPIId(char.location.url);
+    const { character: char, episodes } = useLoaderData() as CharacterDetailsLoaderData;
+    const originId = parseApiUrlId(char.origin.url);
+    const locationId = parseApiUrlId(char.location.url);
 
     const facts: DetailFacts[] = [
         { type: "Species", value: char.species },
         { type: "Status", value: char.status },
         { type: "Gender", value: char.gender },
-        { type: "Origin", value: char.origin.name, url: originId !== -1 ? `/locations/${originId}` : undefined },
-        { type: "Location", value: char.location.name, url: locationId !== -1 ? `/locations/${locationId}` : undefined }
+        { type: "Origin", value: char.origin.name, url: originId !== null ? `/locations/${originId}` : undefined },
+        { type: "Location", value: char.location.name, url: locationId !== null ? `/locations/${locationId}` : undefined }
     ]
-
-    React.useEffect(() => {
-        const episodeList = char.episode.map((episode) => parseAPIId(episode));
-        fetchApiResources<Episode>('episode', episodeList).then(setEpisodes)
-    }, [char.episode])
 
     return (
         <DetailsLayout title={char.name} image={char.image} facts={facts} childrenTitle={"Episodes"}>
-            {episodes.length > 0 ? episodes.map((episode, index) => (
-                <Link to={`/episodes/${episode.id}`} key={index}>
+            {episodes.map((episode) => (
+                <Link to={`/episodes/${episode.id}`} key={episode.id}>
                     <EpisodeCard episode={episode} />
                 </Link>
-            )) : <><LoadingSpinner/> Loading...</>}
+            ))}
         </DetailsLayout>
     );
 }
