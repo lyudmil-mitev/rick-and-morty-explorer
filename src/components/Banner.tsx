278cd9fcd1526695c77136a4888591412e7b14bd
Lyudmil Mitev <lyudmil@mitev.me>
2026-06-22 22:29:15 +0200

    Fix header SVG background loading

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,9 @@
 import mortyLogo from '/android-chrome-512x512.png'
-import starrySpace from '/StarrySpace.svg'
 
 const Banner = () => (
   <section
     className="flex flex-col items-center justify-center bg-cover bg-center px-4 pb-8 pt-6 sm:flex-row sm:pb-10 sm:pt-8"
-    style={{ backgroundImage: `url(${starrySpace})` }}
+    style={{ backgroundImage: `url("${import.meta.env.BASE_URL}StarrySpace.svg")` }}
   >
     <a
       title="Powered by Rick and Morty API!"
